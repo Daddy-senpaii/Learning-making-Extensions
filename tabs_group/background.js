24e6c3303852a6f5de08505911700138ae0ed3cb
Daddy-senpaii <vijaymehra8986@gmail.com>
2025-08-05 18:25:04 +0530

    Writing little of background.js and pop.js to check the configration and validation:

--- tabs_group/background.js
+++ tabs_group/background.js
@@ -1,3 +1,8 @@
 chrome.runtime.onInstalled.addListener(()=> {
     console.log("We are going to start baby");
 })
+
+chrome.runtime.onMessage.addListener((data)=> {
+    console.log(data);
+
+})
--- tabs_group/pop.js
+++ tabs_group/pop.js
@@ -1,1 +1,8 @@
 console.log("Welcome to the pop.js of tabs_grouping");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", ()=> {
+    console.log("button is clicked right know ", );
+
+    chrome.tabs.sendMMessage({action: "GroupTabs" , message: 'message that tell you to group the tabs'});
+})
