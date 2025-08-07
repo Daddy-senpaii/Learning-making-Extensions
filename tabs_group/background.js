e56db29b849ce4b7d2bbc21e6d3f7eb053507870
Daddy-senpaii <vijaymehra8986@gmail.com>
2025-08-07 15:41:37 +0530

    Grouping is done

--- tabs_group/background.js
+++ tabs_group/background.js
@@ -2,10 +2,28 @@ chrome.runtime.onInstalled.addListener(()=> {
     console.log("We are going to start baby");
 })
 
-chrome.runtime.onMessage.addListener((data)=> {
+chrome.runtime.onMessage.addListener(async(data)=> {
     console.log(data.message);
-    if (data.action === "GroupTabs"){
-        console.log("let's now group the tab");
-    }
+    chrome.tabs.query({},(tabs)=> {
+        const domainGroup = {}
+        tabs.forEach((tab)=> {
+            const url = new URL(tab.url);
+            const domain = url.hostname;
+            console.log(domain);
+
+            if(!domainGroup[domain]){
+                domainGroup[domain] = [];
+            }
+            domainGroup[domain].push(tab.id);
+        });
+        console.log(domainGroup);
+        for(const domain in domainGroup){
+            chrome.tabs.group({tabIds: domainGroup[domain]},(groupId)=> {
+                console.log(groupId)
+            })
+        }
+
+        
+    })
 
 })
